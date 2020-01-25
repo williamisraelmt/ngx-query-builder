b82e6922349d9af01f72d38ee9d2e9f186d5b331
Vladyslav Danylovych <vlad.danylovych@gmail.com>
2020-01-25 19:19:31 +0200

    added interfaces to public-api

--- src/ngx-query-builder/src/public-api.ts
+++ src/ngx-query-builder/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ngx-query-builder
  */
 
+export * from './lib/interfaces/ngx-query-builder.interfaces';
 export * from './lib/ngx-query-builder.component';
 export * from './lib/ngx-query-builder.module';
