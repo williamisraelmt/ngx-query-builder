8f2a45ee5a7208e181e55ea053f7e9367acc05f3
Vladyslav Danylovych <vlad.danylovych@gmail.com>
2020-01-26 21:30:26 +0200

    modified operators.service.spec.ts

--- src/ngx-query-builder/src/lib/services/operators.service.spec.ts
+++ src/ngx-query-builder/src/lib/services/operators.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { OperatorsService } from './operators.service';
 
-describe('NgxQueryBuilderService', () => {
+describe('OperatorsService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
